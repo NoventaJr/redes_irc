773ff8c6080732baa335499ddee6754bb9e5ade7
Rodrigo Noventa Júnior <noventajr@gmail.com>
2020-07-17 17:13:57 -0300

    Parte 3 finalizada

--- Include/common.h
+++ Include/common.h
@@ -7,18 +7,6 @@
 #define MAX_MSG_SIZE 4096
 #define QUIT_CMD "/quit"
 
-// //estruturas para conexao
-// struct in_addr{
-//     unsigned long s_addr;
-// };
-
-// struct sockaddr_in{
-//     short sin_family;
-//     unsigned short sin_port;
-//     struct in_addr sin_addr;
-//     char sin_zero[8];
-// };
-
 char *readLine(int *);
 
 #endif
